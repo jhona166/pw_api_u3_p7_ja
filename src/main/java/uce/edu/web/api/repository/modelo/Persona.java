21892673a228084ec4c74ac22704845a8f6e464a
jhona166 <jdaltamiranoo@uce.edu.ec>
2025-01-29 09:41:15 -0500

    quarkus_baseDatos

--- src/main/java/uce/edu/web/api/repository/modelo/Persona.java
+++ src/main/java/uce/edu/web/api/repository/modelo/Persona.java
@@ -1,7 +1,6 @@
 package uce.edu.web.api.repository.modelo;
- 
 import java.time.LocalDateTime;
- 
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -9,50 +8,54 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.SequenceGenerator;
 import jakarta.persistence.Table;
- 
+
 @Entity
 @Table(name = "persona")
-
- 
-   
 public class Persona {
- 
     @Id
+    @GeneratedValue(generator = "seq_persona", strategy = GenerationType.SEQUENCE)
+    @SequenceGenerator(name = "seq_persona", sequenceName = "seq_persona", allocationSize = 1)
     @Column(name = "pers_id")
-    @SequenceGenerator(name = "persona_seq" , sequenceName = "persona_seq", allocationSize = 1)
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "persona_seq")
     private Integer id;
     @Column(name = "pers_nombre")
     private String nombre;
     @Column(name = "pers_apellido")
     private String apellido;
-    @Column(name = "pers_fecha_nacimiento")
+    @Column(name = "pers_fechaNacimiento")
     private LocalDateTime fechaNacimiento;
+
+    // SET Y GET
     public Integer getId() {
         return id;
     }
+
     public void setId(Integer id) {
         this.id = id;
     }
+
     public String getNombre() {
         return nombre;
     }
+
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
+
     public String getApellido() {
         return apellido;
     }
+
     public void setApellido(String apellido) {
         this.apellido = apellido;
     }
+
     public LocalDateTime getFechaNacimiento() {
         return fechaNacimiento;
     }
+
     public void setFechaNacimiento(LocalDateTime fechaNacimiento) {
         this.fechaNacimiento = fechaNacimiento;
     }
- 
- 
-   
+
 }
+
