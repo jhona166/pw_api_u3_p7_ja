c9a2e5958fe2c3bc96f71df0d27c8a0bdd4c81b1
jhona166 <jdaltamiranoo@uce.edu.ec>
2025-02-05 19:53:40 -0500

    taller30

--- src/main/java/uce/edu/web/api/controler/PersonaController.java
+++ src/main/java/uce/edu/web/api/controler/PersonaController.java
@@ -7,6 +7,7 @@ import jakarta.ws.rs.PATCH;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.PUT;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
 import uce.edu.web.api.service.IPersonaService;
 import uce.edu.web.api.service.to.PersonaTo;
 
@@ -18,39 +19,43 @@ public class PersonaController {
     private IPersonaService iPersonaService;
 
     @GET
-    @Path("/buscar")
-    //public Response buscarPorId( ) {
-    public PersonaTo buscarPorId() {
-        Integer id=1;
-        return this.iPersonaService.buscarPorId(id);
-       // return Response.ok(this.iPersonaService.buscarPorId(id)).build();
+    @Path("/{id}")
+    //public Response buscarPorId(@PathParam("id") Integer id ) {
+    public PersonaTo buscarPorId(@PathParam("id") Integer id) {
+       // Integer id=1;
+       return this.iPersonaService.buscarPorId(id);
+       //return Response.ok(this.iPersonaService.buscarPorId(id)).build();
     }
     @POST
-    @Path("/guardar")
+    @Path("")
     //http://localhost:8080/matriculaAPI/v1.1/personas/guardar
     public void guardar(PersonaTo persona) {
         this.iPersonaService.guardar(persona);
     }
     @PUT
-    @Path("/actualizar")
-    public void actualizar(PersonaTo persona) {
+    @Path("/{id}")
+    public void actualizar(PersonaTo persona,@PathParam("id") Integer id ) {
+        persona.setId(id);
         this.iPersonaService.actualizar(persona);
     }
 
     @DELETE
-    @Path("/borrar")
-    public void borrar() {
-        Integer id = 1;
+    @Path("/{id}")
+    public void borrar(PersonaTo persona,@PathParam("id") Integer id) {
+       
         this.iPersonaService.borrar(id);
     }
     //Poner anotacion a los objetods que van a viajar en el request 
 
     @PATCH
-    @Path("/actualizar/parcial")
-    public void actualizarParcial(PersonaTo persona) {
+    @Path("/{id}/nuevo/{cedula}")
+    public void actualizarParcial(PersonaTo persona,@PathParam("id") Integer id,@PathParam("cedula") String cedula) {
+        System.err.println(cedula);
         PersonaTo tmp =this.iPersonaService.buscarPorId(persona.getId());
         tmp.setNombre(persona.getNombre());
         this.iPersonaService.actualizar(tmp);
+
+        
     }
 
 }
